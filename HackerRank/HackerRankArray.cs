2880e3cf5b056400590a1bc6ec13d92255e6a790
Ismailsharief Babu <M1045935@mindtree.com>
2020-02-23 15:25:53 +0530

    Commiting HackerRank Array Challenge Programming code as on 02/23

---
+++ HackerRank/HackerRankArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRank
+{
+	[TestClass]
+	public class HackerRankArray
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			int n = 4;
+			int[] arr = { 1, 4, 3, 2 };
+			//Console.WriteLine(arr.Length);
+			for(int i=n-1; i>=0; i--)
+			{
+				Console.Write(arr[i]);
+				if(i==0)
+				{
+					break;
+				}
+				else
+				{
+					Console.Write(" ");
+				}
+			}
+		}
+	}
+}

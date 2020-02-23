5de9c2e165f63f7d2328428bd414e65c82fc843a
Ismailsharief Babu <M1045935@mindtree.com>
2020-02-23 14:31:07 +0530

    Commiting all local changes includes HackerRank Challenge Programs.

---
+++ HackerRank/HackerRankTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRank
+{
+	[TestClass]
+	public class HackerRankTest
+	{
+		[TestMethod]
+		public void DataTypeTest()
+		{
+			/*
+			double d = 4.0;
+			double e = 8.0;
+			double f = d + e;
+
+			Console.WriteLine(f.ToString("0.0"));
+			
+			double meal = 12.00;
+			double tip = (20/(double)100) * meal;
+			double tax = (8 / (double)100) * meal;
+
+			double bill = tip + tax + meal;
+			
+			Console.WriteLine("Tip: "+tip);
+			Console.WriteLine("Tax: " +tax);
+			Console.WriteLine(Convert.ToInt16(bill));
+			
+			int n = 4;
+			for(int i=1; i<=10; i++)
+			{
+				Console.WriteLine(n + " x " + i + " = " + n * i);
+			}
+			*/
+
+			int T = 1;
+
+			for(int i=0; i<=T-1; i++)
+			{
+				string S = "Hacker";
+				for(int j=0; j<S.Length; j++)
+				{
+					if(j%2 == 0)
+					{
+						Console.Write(S[j]);
+					}
+				}
+				Console.Write("  ");
+				for (int k = 0; k < S.Length; k++)
+				{
+					if (k % 2 != 0)
+					{
+						Console.Write(S[k]);
+					}
+				}
+
+			}
+
+
+		}
+	}
+}
---
+++ HackerRank/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HackerRank")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HackerRank")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84cce98b-057f-497b-9309-a4a4e8a755d4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
